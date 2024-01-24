0c4751a74bdbaa1d71dca09d6c6315fbf4550332
jon1267 <jonleev@gmail.com>
2024-01-24 19:54:59 +0200

    delivery addr && checkout start

--- resources/js/Pages/User/CartList.vue
+++ resources/js/Pages/User/CartList.vue
@@ -1,7 +1,12 @@
 <script setup>
 import { computed } from 'vue';
-import { router, usePage } from "@inertiajs/vue3";
+import { router, usePage, Link } from "@inertiajs/vue3";
 import UserLayouts from './Layouts/UserLayouts.vue';
+
+// Lesson 15 TIME 2:36
+defineProps({
+  userAddress: Object
+})
 
 const carts = computed(() => usePage().props.cart.data.items);
 const products = computed(() => usePage().props.cart.data.products);
@@ -108,19 +113,31 @@ const remove = (product) => {
           <p class="leading-relaxed mb-5 text-gray-600">or you can add new below</p>
 
           <div class="relative mb-4">
-            <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+            <label for="name" class="leading-7 text-sm text-gray-600">Address 1</label>
             <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
           </div>
           <div class="relative mb-4">
-            <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+            <label for="email" class="leading-7 text-sm text-gray-600">Sity</label>
             <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
           </div>
           <div class="relative mb-4">
-            <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            <label for="message" class="leading-7 text-sm text-gray-600">State</label>
+            <input type="text" id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <div class="relative mb-4">
+            <label for="zipcode" class="leading-7 text-sm text-gray-600">Zipcode</label>
+            <input type="text" id="zipcode" name="zipcode" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <div class="relative mb-4">
+            <label for="country_code" class="leading-7 text-sm text-gray-600">Country Code</label>
+            <input type="text" id="country_code" name="country_code" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+          </div>
+          <div class="relative mb-4">
+            <label for="address_type" class="leading-7 text-sm text-gray-600">Address Type</label>
+            <input type="text" id="address_type" name="address_type" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-6 transition-colors duration-200 ease-in-out">
           </div>
           <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Checkout</button>
-          <p class="text-xs text-gray-500 mt-3">Continue shopping.</p>
+          <Link :href="route('products.index')" class="text-sm text-gray-500 mt-3 hover:text-gray-600 hover:font-bold">Continue shopping.</Link>
 
         </div>
       </div>
